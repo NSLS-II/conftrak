c04b7decf2874d28e500d8424946c5ecbfde397b
Jun Aishima <jaishima@bnl.gov>
2021-12-22 15:27:16 -0500

    add version info

--- setup.py
+++ setup.py
@@ -2,6 +2,27 @@ __author__ = 'hhslepicka'
 import setuptools
 import versioneer
 import os
+import sys
+
+# NOTE: This file must remain Python 2 compatible for the foreseeable future,
+# to ensure that we error out properly for people with outdated setuptools
+# and/or pip.
+min_version = (3, 6)
+if sys.version_info < min_version:
+    error = """
+mxtools does not support Python {0}.{1}.
+Python {2}.{3} and above is required. Check your Python version like so:
+
+python3 --version
+
+This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
+Upgrade pip like so:
+
+pip install --upgrade pip
+""".format(
+        *(sys.version_info[:2] + min_version)
+    )
+    sys.exit(error)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,6 +45,7 @@ setuptools.setup(
     install_requires=requirements,
     license="BSD 3-Clause",
     url="https://github.com/hhslepicka/conftrak.git",
+    python_requires=">={}".format(".".join(str(n) for n in min_version)),
     packages=setuptools.find_packages(),
     package_data={'conftrak': ['schemas/*.json']},
     classifiers=[
