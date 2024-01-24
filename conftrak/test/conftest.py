a29479fc107a9cec2dca8f17e0d9119311de8886
Jun Aishima <jaishima@bnl.gov>
2024-01-24 14:00:39 -0500

    make conftest the central place for all tests
     * add fixture for service instance
     * start with simple initial tests

--- conftrak/test/conftest.py
+++ conftrak/test/conftest.py
@@ -1,17 +1,19 @@
 import pytest
 
-from ..ignition import start_server
+from conftrak.ignition import start_server
 import uuid
 import sys
 import time as ttime
 import subprocess
 import contextlib
+from conftrak.client.commands import ConfigurationReference
+
+testing_config = dict(mongo_uri='mongodb://localhost', mongo_host="localhost",
+                    database='conftrak_test'+str(uuid.uuid4()), serviceport=7771,
+                    tzone='US/Eastern', log_file_prefix="", testing=True)
 
 @contextlib.contextmanager
 def conftrak():
-    testing_config = dict(mongo_uri='mongodb://localhost',
-                      database='conftrak_test'+str(uuid.uuid4()), serviceport=7771,
-                      tzone='US/Eastern')
     try:
         ps = subprocess.Popen(
             [
@@ -29,4 +31,16 @@ def conftrak():
 @pytest.fixture(scope="session")
 def conftrak_fixture():
     with conftrak() as conftrak_fixture:
+        print(conftrak_fixture)
         yield
+
+@pytest.fixture(scope='function')
+def conftrak_client():
+    c = ConfigurationReference(host=testing_config["mongo_host"],port=testing_config["serviceport"])
+    return c
+
+def test_conftrak_fixture(conftrak_fixture, conftrak_client):
+    print(conftrak_client)
+    print(conftrak_client.get_schema())
+    print(list(conftrak_client.find()))
+
